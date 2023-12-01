9a759947e81f2f97fc6627faab8bd15a5861153d
unknown <yashika.agrawal23@gmail.com>
2023-12-02 00:28:25 +0530

    d

--- React Practice/dummy/src/Component/header.js
+++ React Practice/dummy/src/Component/header.js
@@ -1,3 +1,22 @@
-const Header = () => {
-    
-}
+import { useState } from "react";
+
+const Header = ({ country }) => {
+  const [state, setState] = useState("Yashika");
+  let state1 = "Tony Stark"; // not re rerender on change
+  return (
+    <div>
+      <h1>
+        Hello, {state} from {country}
+      </h1>
+      <button
+        onClick={() => {
+          setState("Yash");
+        }}
+      >
+        Change Name
+      </button>
+    </div>
+  );
+};
+
+export default Header;
