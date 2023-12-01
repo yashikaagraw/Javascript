5d1894c45116e6d83af748ccf3ff31b93934b4aa
unknown <yashika.agrawal23@gmail.com>
2023-12-02 00:28:51 +0530

    basics

--- React Practice/fetchpost.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const fetchpost = () => {
-  return (
-    <div>fetchpost</div>
-  )
-}
-
-export default fetchpost
--- jsunderstanding.js
+++ jsunderstanding.js
@@ -12,3 +12,44 @@ add(10, 15); recognise arguments
     return x+y;
  }
  add(10, 15) explain arguments 
+ ##type of null => object
+##type of nan => number
+undefined means a variable has not been defined or declared 
+null- null is a special value that represents an empty or unknown value. For example, let number = null;
+== in JavaScript is used for comparing two variables, but it ignores the datatype of variable.
+=== is used for comparing two variables, but this operator also checks datatype and compares two values. 
+##Checks the equality of two operands without considering their type
+##
+A JavaScript function is defined with the function keyword, followed by a name, followed by parentheses ().
+
+Function names can contain letters, digits, underscores, and dollar signs (same rules as variables).
+
+The parentheses may include parameter names separated by commas:
+(parameter1, parameter2, ...)
+##
+An array is a special variable, which can hold more than one value:
+## Objects:-
+In JavaScript, almost "everything" is an object.
+
+Booleans can be objects (if defined with the new keyword)
+Numbers can be objects (if defined with the new keyword)
+Strings can be objects (if defined with the new keyword)
+Dates are always objects
+Maths are always objects
+Regular expressions are always objects
+Arrays are always objects
+Functions are always objects
+Objects are always objects
+
+##Hoisting:- hoisting is a phenomenon in javascript by which 
+you can access these variables and functions even before you
+have initialized it and you have put same value in it you can
+access it without any error
+
+#Scoping:- 3 types of scope:-
+block(let and const), function and global
+
+## Promises:-
+
+
+
