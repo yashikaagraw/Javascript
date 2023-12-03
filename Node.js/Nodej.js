92446c39729822d399cd40e117112e16697b6c78
unknown <yashika.agrawal23@gmail.com>
2023-12-03 17:09:10 +0530

    node.js

---
+++ Node.js/Nodej.js
@@ -0,0 +1,43 @@
+The package.json file is the heart of any Node project.
+In package.json file we keep all the details of the projects that hold various metadata relevant to the project like version
+description, dependencies, run scripts, license and all npm packages.
+without this package, your project will not work 
+
+#################################3
+Node.js is a open source server enviroment. 
+Node.js is a platform built on chrome's javascript runtime for easily building fast and scalable network applications.
+it is a single threaded, non-blocking and event driven 
+
+#################################
+Node.js used an event loop that handles the asynchronous task
+The event loop makes our program nonblocking.
+We can handle multiple request at a time in parallel- that's the reason node js is fast 
+
+####################################
+EVENT LOOP :-
+The event loop makes the code async.
+behind the scene when our code asynchronously then there are containers that are 
+call-stack
+call-back queue,
+web API and 
+micro task queue
+
+The purpose of event loop is to execute the async task as per priority.
+
+####################################
+WHEN TO USE NODE.js:-
+Considering the main features of node.js and its strength, we can say 
+data-driven, I/O- driven, event-driven and nonblocking applications benfit the best from a Mode.js
+implementation.
+
+It can be used to create :-
+web servers
+Real time applications (chat applications, streaming, online games)
+command line application 
+creating applications
+
+#################################################33
+EXPRESS.JS 
+Express is a node.js web application framework that provides broad features
+for building web and mobile applications
+It's a layer built on top of the node js that helps manager servers and routes 
